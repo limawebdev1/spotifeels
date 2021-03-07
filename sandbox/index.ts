7ac150b971cd486a4fa80615231eff94a62d82d0
Lisa Ma <lisa@yonomi.co>
2021-03-07 16:33:55 -0600

    sandboxing spotify api discovery

---
+++ sandbox/index.ts
@@ -0,0 +1,47 @@
+import dotenv from 'dotenv';
+import SpotifyWebApi from 'spotify-web-api-node';
+import axios from 'axios';
+import querystring from 'querystring';
+
+dotenv.config();
+let spotifyApi: SpotifyWebApi;
+
+interface AppCredentials {
+  clientId: string;
+  clientSecret: string;
+}
+
+const loadAppCredentials = (): AppCredentials => {
+  const { SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET } = process.env;
+  if (!SPOTIFY_CLIENT_ID || !SPOTIFY_CLIENT_SECRET) {
+    throw new Error('Missing Spotify app credentials');
+  }
+  return {
+    clientId: SPOTIFY_CLIENT_ID,
+    clientSecret: SPOTIFY_CLIENT_SECRET,
+  }
+}
+
+const authorizeSpotify = async () => {
+  const { clientId, clientSecret } = loadAppCredentials();
+  spotifyApi = new SpotifyWebApi({ clientId, clientSecret });
+  const { body } = await spotifyApi.clientCredentialsGrant();
+  spotifyApi.setAccessToken(body.access_token);
+}
+
+const getTracks = async () => {
+  try {
+    const { body: { tracks }} = await spotifyApi.getRecommendations({
+      min_energy: 0.4,
+      seed_artists: ['6mfK6Q2tzLMEchAr0e9Uzu', '4DYFVNKZ1uixa6SQTvzQwJ']
+    });
+    console.log(tracks);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+(async () => {
+  await authorizeSpotify();
+  await getTracks();
+})();
